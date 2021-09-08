2921193e941947a4e0b5b1c4859594a3f1479801
Timur <t.tokumov@gmail.com>
2021-09-09 05:40:41 +0600

    Added new code

--- src/view/edit-form.js
+++ src/view/edit-form.js
@@ -3,22 +3,61 @@ import { createFormTemplate } from '../form-template.js';
 import { generateOffer } from '../mocks/generate-offer.js';
 import { generateDescription, generateRandomPhoto } from '../mocks/generate-destination.js';
 import { mocksConstants } from '../mocks/mock-constants.js';
+import flatpickr from 'flatpickr';
+
+import '../../node_modules/flatpickr/dist/flatpickr.min.css';
+
 
 export default class EditForm extends SmartView {
   constructor(path) {
     super();
     this._data = EditForm.parsePathToData(path);
+    this._datepicker = null;
+
     this._resetButtonName = 'Delete';
     this._clickHandler = this._clickHandler.bind(this);
     this._formHandler = this._formHandler.bind(this);
     this._changeTypeClickHandler = this._changeTypeClickHandler.bind(this);
     this._changeDestinationClickHandler = this._changeDestinationClickHandler.bind(this);
+    this._dueDateChangeHandler = this._dueDateChangeHandler.bind(this);
 
     this._setInnerHandlers();
+    this._setDatepicker();
   }
 
   getTemplate() {
     return createFormTemplate(this._data, this._resetButtonName);
+  }
+
+  _setDatepicker() {
+    if (this._datepicker) {
+      this._datepicker.destroy();
+      this._datepicker = null;
+    }
+
+    this._datepicker = flatpickr(
+      this.getElement().querySelector('#event-start-time-1'),
+      {
+        dateFormat: 'd/m/Y',
+        defaultDate: this._data.dateFrom,
+        onChange: this._dueDateChangeHandler,
+      },
+    );
+
+    this._datepicker = flatpickr(
+      this.getElement().querySelector('#event-end-time-1'),
+      {
+        dateFormat: 'd/m/Y',
+        defaultDate: this._data.dateFrom,
+        onChange: this._dueDateChangeHandler,
+      },
+    );
+  }
+
+  _dueDateChangeHandler([userDate]) {
+    this.updateData({
+      dateFrom: userDate.format('DD/MM/YY HH:MM'),
+    });
   }
 
   reset(path) {
@@ -31,6 +70,7 @@ export default class EditForm extends SmartView {
     this._setInnerHandlers();
     this.setEditClickHandler(this._callback.editClick);
     this.setEditSubmitHandler(this._callback.formClick);
+    this._setDatepicker();
   }
 
   _setInnerHandlers() {
