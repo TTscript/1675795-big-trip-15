3dbb810a5b371e8534a957a82a4af11b57fcf30a
Timur <t.tokumov@gmail.com>
2021-09-12 03:38:08 +0600

    Added changes

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 import { generatePath } from './mocks/generate-path.js';
 import PathListView from './view/path-list.js';
 
-const TASK_COUNT = 1;
+const TASK_COUNT = 20;
 const paths = new Array(TASK_COUNT).fill().map(generatePath);
 const pathListComponent = new PathListView();
 
